aa955591b80bb8cac572b44bf80725a584ade0ff
Hirotaka Kawashima <k.hirotaka@me.com>
2020-01-18 12:41:07 +0900

    add hard & soft ensemble

---
+++ enchanter/estimator/ensemble.py
@@ -0,0 +1,79 @@
+from typing import List
+
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+from sklearn.base import BaseEstimator
+
+from .runner import BaseRunner, ClassificationRunner
+from . import modules
+
+if modules.is_jupyter():
+    from tqdm.notebook import tqdm
+else:
+    from tqdm import tqdm
+
+
+class BaseEnsembleEstimator(BaseEstimator):
+    def __init__(self, runners: List[BaseRunner], mode: str = None):
+        self.runners: List[BaseRunner] = runners
+        self.weights: List = []
+        self.mode: str = mode
+        self.do_fit: bool = False
+
+    def fit(self, dataset: Dataset, epochs: int, batch_size: int, shuffle: bool = True, checkpoint: str = False):
+        self.do_fit = True
+        for runner in tqdm(self.runners, desc="Runner"):
+            runner.fit(dataset, epochs, batch_size, shuffle, checkpoint)
+            self.weights.append(runner.save_checkpoint()["model_state_dict"])
+
+        return self
+
+    def predict(self, x):
+        raise NotImplementedError
+
+
+class SoftEnsemble(BaseEnsembleEstimator):
+    """
+    確率の平均をとるアンサンブル
+    """
+    def __init__(self, models: List[BaseRunner], mode: str = None):
+        super(SoftEnsemble, self).__init__(models, mode)
+
+    def predict(self, x) -> np.ndarray:
+        if isinstance(x, np.ndarray):
+            x = torch.from_numpy(x)
+
+        predicts = []
+        for i, runner in enumerate(tqdm(self.runners, desc="Runner")):
+            if self.do_fit:
+                runner.model.load_state_dict(self.weights[i])
+            predicts.append(runner.predict(x))
+
+        predicts = sum(predicts)
+        probs = predicts / len(self.runners)
+
+        if self.mode == "classification":
+            return probs.astype(np.int)
+        else:
+            return probs
+
+
+class HardEnsemble(BaseEnsembleEstimator):
+    """
+    多数決をとるアンサンブル
+    """
+    def __init__(self, models: List[ClassificationRunner]):
+        super(HardEnsemble, self).__init__(models, mode="classification")
+
+    def predict(self, x) -> np.ndarray:
+        if isinstance(x, np.ndarray):
+            x = torch.from_numpy(x)
+
+        predicts = []
+        for runner in self.runners:
+            predicts.append(runner.predict(x))
+
+        predicts = np.stack(predicts)
+
+        return np.ravel(predicts[0])
---
+++ test/estimator/ensamble/soft_ensemble.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
+
+from enchanter.estimator.ensemble import SoftEnsemble
+from enchanter.estimator.runner import ClassificationRunner
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(28 * 28, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 10)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = x.view(-1, 28*28)
+        x = self.fc(x)
+        return x
+
+
+def main():
+    train_ds = MNIST("../../data", train=True, download=False, transform=ToTensor())
+    test_ds = MNIST("../../data", train=False, download=False, transform=ToTensor())
+
+    runner1 = ClassificationRunner(Model(), nn.CrossEntropyLoss(), optim.Adam, optim_conf={"lr": 0.001})
+    runner2 = ClassificationRunner(Model(), nn.CrossEntropyLoss(), optim.Adam, optim_conf={"lr": 0.002})
+    runner3 = ClassificationRunner(Model(), nn.CrossEntropyLoss(), optim.Adam, optim_conf={"lr": 0.003})
+
+    ensemble = SoftEnsemble([runner1, runner2, runner3], "classification")
+    ensemble.fit(train_ds, epochs=1, batch_size=32)
+
+    img, label = next(iter(DataLoader(test_ds, batch_size=32)))
+    print("predict: ", ensemble.predict(img).astype("int"))
+    print("label", label)
+
+
+if __name__ == '__main__':
+    main()
