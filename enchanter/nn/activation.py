714a6e975404343b0518b8651485f9d180110e09
Hirotaka Kawashima <k.hirotaka@me.com>
2020-01-18 13:14:07 +0900

    create swish

---
+++ enchanter/nn/activation.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+
+class Swish(nn.Module):
+    def __init__(self, beta: bool = False):
+        super(Swish, self).__init__()
+        if beta:
+            self.weight = nn.Parameter(torch.tensor([1.], dtype=torch.float32), requires_grad=True)
+        else:
+            self.weight = 1.0
+
+    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
+        out = inputs * torch.sigmoid(self.weight * inputs)
+        return out
