6f95f1966a20f27cc382a6b9489c64f4793d0556
Hirotaka Kawashima <k.hirotaka@me.com>
2020-01-18 21:16:37 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='enchanter',
+    version='0.1',
+    packages=['enchanter', 'enchanter.nn', 'enchanter.metrics', 'enchanter.estimator'],
+    url='https://github.com/khirotaka/enchanter',
+    license='Apache-2.0',
+    author='Hirotaka Kawashima',
+    author_email='',
+    description='Machine Learning Pipeline, Training and Logging for Me.'
+)
