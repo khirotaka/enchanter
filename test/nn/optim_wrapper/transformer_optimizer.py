723772cf44dfc30e86fd36f7508243431ba67cb7
Hirotaka Kawashima <k.hirotaka@me.com>
2020-01-19 10:07:38 +0900

    create optimizer wrapper (Transformer)

---
+++ test/nn/optim_wrapper/transformer_optimizer.py
@@ -0,0 +1,29 @@
+import torch
+import torch.jit
+import torch.nn as nn
+import torch.optim as optim
+from enchanter.nn.optim_wrapper import TransformerOptimizer
+
+
+def main():
+    x = torch.randn(64, 32)
+    y = torch.randint(0, 10, [64, ])
+
+    model = nn.Sequential(
+        nn.Linear(32, 16),
+        nn.ReLU(),
+        nn.Linear(16, 10)
+    )
+
+    optimizer = TransformerOptimizer(optim.Adam(model.parameters()), 16, 10)
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer.zero_grad()
+    out = model(x)
+    loss = criterion(out, y)
+    loss.backward()
+    optimizer.step()
+
+
+if __name__ == '__main__':
+    main()
