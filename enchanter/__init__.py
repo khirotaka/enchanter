a027581c5fa88a90888ed97204532b5676d80f70
Hirotaka Kawashima <k.hirotaka@me.com>
2020-02-27 23:13:25 +0900

    Update __init__.py

--- enchanter/__init__.py
+++ enchanter/__init__.py
@@ -3,6 +3,6 @@ from .engine import modules
 from .__version__ import __version__
 
 
-del Dict, Union, Any, List
+del Dict
 del io, os, time, deepcopy
-del np, torch, tqdm, DataLoader, Dataset, base, metrics
+del torch, tqdm, DataLoader, base
