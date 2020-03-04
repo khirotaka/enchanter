b6e10501e581be6daaab51d349513bb837c12094
Hirotaka Kawashima <k.hirotaka@me.com>
2020-03-04 21:40:00 +0900

    Create classification.py

---
+++ tests/wrappers/classification.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.datasets import load_iris
+from torch.utils.data import DataLoader
+
+import enchanter.wrappers as wrappers
+import enchanter.addons as addons
+import enchanter.engine as engine
+from enchanter.callbacks import TensorBoardLogger
+
+
+x, y = load_iris(return_X_y=True)
+ds = engine.get_dataset(x, y)
+loader = DataLoader(ds, batch_size=32, shuffle=True)
+
+model = nn.Sequential(
+    nn.Linear(4, 16),
+    addons.Mish(),
+    nn.Linear(16, 3)
+)
+optimizer = optim.Adam(model.parameters())
+
+
+def test_classification():
+    runner = wrappers.ClassificationRunner(
+        model,
+        optimizer,
+        nn.CrossEntropyLoss(),
+        TensorBoardLogger("./logs")
+    )
+    runner.add_loader("train", loader)
+    runner.train_config(epochs=1)
+
+    try:
+        runner.run(verbose=False)
+        is_pass = True
+    except Exception:
+        is_pass = False
+
+    assert is_pass
