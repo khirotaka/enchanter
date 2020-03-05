12509476b0f1a0f6d0dd2a7fc9f9a69411835d86
Hirotaka Kawashima <k.hirotaka@me.com>
2020-03-05 10:23:39 +0900

    rename engine/modules -> engine/test_modules

--- tests/engine/test_modules.py
+++ tests/engine/test_modules.py
@@ -7,13 +7,13 @@ import enchanter.engine as engine
 def test_numpy2tensor_1():
     x = torch.randn(2)
     y = engine.numpy2tensor(x)
-    assert y is torch.Tensor
+    assert isinstance(y, torch.Tensor)
 
 
 def test_numpy2_tensor_2():
     x = np.random.randn(2)
     y = engine.numpy2tensor(x)
-    assert y is torch.Tensor
+    assert isinstance(y, torch.Tensor)
 
 
 def test_get_dataset_1():
@@ -21,11 +21,11 @@ def test_get_dataset_1():
     y = torch.randint(0, 9, (32, ))
 
     ds = engine.get_dataset(x, y)
-    return ds is Dataset
+    return isinstance(ds, Dataset)
 
 
 def test_get_dataset_2():
     x = torch.randn(32, 128)
 
     ds = engine.get_dataset(x)
-    return ds is Dataset
+    return isinstance(ds, Dataset)
