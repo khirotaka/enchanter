18b032266b37b3d59a81b07b871e88123a6e421f
Hirotaka Kawashima <k.hirotaka@me.com>
2020-03-09 13:02:29 +0900

    refactoring

--- tests/wrappers/test_classification.py
+++ tests/wrappers/test_classification.py
@@ -7,26 +7,22 @@ from torch.utils.data import DataLoader
 
 import enchanter.wrappers as wrappers
 import enchanter.addons as addons
-import enchanter.engine as engine
 from enchanter.callbacks import TensorBoardLogger
+from enchanter.engine.modules import get_dataset
 
 x, y = load_iris(return_X_y=True)
 x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
 x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, random_state=0)
 
-train_ds = engine.modules.get_dataset(x_train.astype(np.float32), y_train.astype(np.int64))
-val_ds = engine.modules.get_dataset(x_val.astype(np.float32), y_val.astype(np.int64))
-test_ds = engine.modules.get_dataset(x_test.astype(np.float32), y_test.astype(np.int64))
+train_ds = get_dataset(x_train.astype(np.float32), y_train.astype(np.int64))
+val_ds = get_dataset(x_val.astype(np.float32), y_val.astype(np.int64))
+test_ds = get_dataset(x_test.astype(np.float32), y_test.astype(np.int64))
 
 train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
-val_loader = DataLoader(val_ds, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_ds, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_ds, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_ds, batch_size=32, shuffle=False)
 
-model = nn.Sequential(
-    nn.Linear(4, 16),
-    addons.Mish(),
-    nn.Linear(16, 3)
-)
+model = addons.layers.MLP([4, 16, 3], activation=addons.Mish())
 optimizer = optim.Adam(model.parameters())
 
 
