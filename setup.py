868cd54362886b8c0e75103af944f70625ec7622
Hirotaka Kawashima <k.hirotaka@me.com>
2020-03-11 20:41:59 +0900

    v0.3.1 -> v0.3.2

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def load_requirements(path_dir=PATH_ROOT, comment_char='#'):
 
 setup(
     name='enchanter',
-    version='0.3.1',
+    version='0.3.2',
     packages=[
         'enchanter', 'enchanter.addons', 'enchanter.addons.layers', 'enchanter.callbacks',
         'enchanter.engine', 'enchanter.metrics', 'enchanter.wrappers'
