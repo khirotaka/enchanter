ecfd2b0f2bfbfc6dd0e32ef5c762b30004600a12
Hirotaka Kawashima <k.hirotaka@me.com>
2020-03-16 21:59:48 +0900

    update addons/layers/__init__

--- enchanter/addons/layers/__init__.py
+++ enchanter/addons/layers/__init__.py
@@ -1,7 +1,10 @@
 from .positional_encoding import PositionalEncoding
 from .mlp import MLP
 from .mlp import PositionWiseFeedForward
+from .se_layer import SELayer1d
+from .se_layer import SELayer2d
+from .dense_interpolation import DenseInterpolation
 
 __all__ = [
-    "PositionalEncoding", "MLP", "PositionWiseFeedForward"
+    "PositionalEncoding", "MLP", "PositionWiseFeedForward", "SELayer1d", "SELayer2d", "DenseInterpolation"
 ]
--- enchanter/addons/layers/__init__.pyi
+++ enchanter/addons/layers/__init__.pyi
@@ -1,3 +1,6 @@
 from .mlp import MLP as MLP
 from .mlp import PositionWiseFeedForward as PositionWiseFeedForward
 from .positional_encoding import PositionalEncoding as PositionalEncoding
+from .se_layer import SELayer1d as SELayer1d
+from .se_layer import SELayer2d as SELayer2d
+from .dense_interpolation import DenseInterpolation as DenseInterpolation
