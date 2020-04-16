e9b3e7ac664b4827ec33aaf929bfff9d866c1dcf
Hirotaka Kawashima <k.hirotaka@me.com>
2020-04-16 10:43:36 +0900

    出力されるデータ型のヒントを修正

--- enchanter/metrics/classification.py
+++ enchanter/metrics/classification.py
@@ -25,7 +25,7 @@ def calculate_accuracy(inputs, targets):
         targets (torch.Tensor): shape == [N]
 
     Returns:
-        分類精度 (torch.Tensor)
+        分類精度 (float)
     """
     with torch.no_grad():
         total = targets.shape[0]
