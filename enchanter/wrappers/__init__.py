0e884dfb0f49588a40c5fd28df00ec0f45b2075f
Hirotaka Kawashima <k.hirotaka@me.com>
2020-04-20 19:13:56 +0900

    add BaseEnsebleEstimator

--- enchanter/wrappers/__init__.py
+++ enchanter/wrappers/__init__.py
@@ -1,8 +1,8 @@
 from .classification import ClassificationRunner
 from .regression import RegressionRunner
-from .ensemble import HardEnsemble, SoftEnsemble
+from .ensemble import HardEnsemble, SoftEnsemble, BaseEnsembleEstimator
 
 __all__ = [
     "ClassificationRunner", "RegressionRunner",
-    "HardEnsemble", "SoftEnsemble"
+    "HardEnsemble", "SoftEnsemble", "BaseEnsembleEstimator"
 ]
--- enchanter/wrappers/__init__.pyi
+++ enchanter/wrappers/__init__.pyi
@@ -1,6 +1,6 @@
 from .classification import ClassificationRunner as ClassificationRunner
 from .regression import RegressionRunner as RegressionRunner
-from .ensemble import HardEnsemble as HardEnsemble, SoftEnsemble as SoftEnsemble
+from .ensemble import HardEnsemble as HardEnsemble, SoftEnsemble as SoftEnsemble, BaseEnsembleEstimator as BaseEnsembleEstimator
 
 # Names in __all__ with no definition:
 #   ClassificationRunner
