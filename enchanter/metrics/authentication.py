f322c8932b35f8a9a18087ee5d7ec087d1494ad6
Hirotaka Kawashima <k.hirotaka@me.com>
2020-04-29 21:04:31 +0900

    refactoring

--- enchanter/metrics/authentication.py
+++ enchanter/metrics/authentication.py
@@ -24,6 +24,6 @@ def calculate_eer(y_true, y_score):
     Returns:
         return EER Score
     """
-    fpr, tpr, thresholds = roc_curve(y_true, y_score, pos_label=1)
+    fpr, tpr, _ = roc_curve(y_true, y_score, pos_label=1)
     eer_score = brentq(lambda x: 1. - x - interp1d(fpr, tpr)(x), 0., 1.)
     return eer_score
