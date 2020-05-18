975b79e8491aafe1c9ac12fa5afc9652dd5b0157
Hirotaka Kawashima <k.hirotaka@me.com>
2020-05-18 22:01:16 +0900

    Update test_modules.py

--- tests/engine/test_modules.py
+++ tests/engine/test_modules.py
@@ -4,18 +4,6 @@ from torch.utils.data import Dataset
 import enchanter.engine.modules as modules
 
 
-def test_numpy2tensor_1():
-    x = torch.randn(2)
-    y = modules.numpy2tensor(x)
-    assert isinstance(y, torch.Tensor)
-
-
-def test_numpy2_tensor_2():
-    x = np.random.randn(2)
-    y = modules.numpy2tensor(x)
-    assert isinstance(y, torch.Tensor)
-
-
 def test_get_dataset_1():
     x = torch.randn(32, 128)
     y = torch.randint(0, 9, (32, ))
