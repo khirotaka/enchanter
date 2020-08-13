23a7cab439dd4911af09d0a42dd346a5a34330cb
Hirotaka Kawashima <k.hirotaka@me.com>
2020-08-13 19:48:29 +0900

    rc7

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='enchanter',
-    version='0.6.0rc5',
+    version='0.6.0rc7',
     packages=find_packages(exclude=["docker", "docs", "examples", "tests", "tests.*"]),
     url='https://github.com/khirotaka/enchanter',
     keywords="pytorch comet_ml",
