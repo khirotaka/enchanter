8a7dfb3acebeb386e8b9a0de2fdf792f261433c7
Hirotaka Kawashima <k.hirotaka@me.com>
2020-09-07 12:15:13 +0900

    refactord CausalConv1d

--- enchanter/addons/layers/conv.py
+++ enchanter/addons/layers/conv.py
@@ -9,14 +9,14 @@ __all__ = ["CausalConv1d"]
 
 class CausalConv1d(nn.Conv1d):
     def __init__(
-            self,
-            in_channels: int,
-            out_channels: int,
-            kernel_size: Union[int, tuple],
-            stride: int = 1,
-            dilation: int = 1,
-            groups: int = 1,
-            bias: bool = True
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: Union[int, tuple],
+        stride: int = 1,
+        dilation: int = 1,
+        groups: int = 1,
+        bias: bool = True,
     ) -> None:
         """
         Causal Conv1d
@@ -29,16 +29,10 @@ class CausalConv1d(nn.Conv1d):
             dilation: rate of dilation
             groups: the numer of gropus
             bias: if true use bias (default: True)
+
         """
         super(CausalConv1d, self).__init__(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride,
-            padding=0,
-            dilation=dilation,
-            groups=groups,
-            bias=bias
+            in_channels, out_channels, kernel_size, stride, padding=0, dilation=dilation, groups=groups, bias=bias
         )
         self.left_padding: int = (kernel_size - 1) * dilation
 
