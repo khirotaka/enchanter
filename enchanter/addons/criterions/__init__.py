3079a2b3b495e7ae9cffaa221c4a2b789ec928e3
Hirotaka Kawashima <k.hirotaka@me.com>
2020-09-26 14:02:35 +0900

    apply black

--- enchanter/addons/criterions/__init__.py
+++ enchanter/addons/criterions/__init__.py
@@ -2,7 +2,11 @@ from .ts_triplet_loss import *
 
 
 __all__ = [
-    "ts_triplet_loss", "generate_sample_indices", "generate_anchor_positive_input",
-    "generate_negative_input", "positive_criterion_for_triplet_loss", "negative_criterion_for_triplet_loss",
-    "calculate_triplet_loss"
+    "ts_triplet_loss",
+    "generate_sample_indices",
+    "generate_anchor_positive_input",
+    "generate_negative_input",
+    "positive_criterion_for_triplet_loss",
+    "negative_criterion_for_triplet_loss",
+    "calculate_triplet_loss",
 ]
