5e38b057ce3236ef823f4f3d73fe53b9bc710f93
Hirotaka Kawashima <k.hirotaka@me.com>
2020-10-02 21:21:05 +0900

    apply black

--- enchanter/addons/criterions/ts_triplet_loss.py
+++ enchanter/addons/criterions/ts_triplet_loss.py
@@ -7,8 +7,12 @@ import torch.jit
 
 
 __all__ = [
-    "generate_sample_indices", "generate_anchor_positive_input", "generate_negative_input",
-    "positive_criterion_for_triplet_loss", "negative_criterion_for_triplet_loss", "calculate_triplet_loss"
+    "generate_sample_indices",
+    "generate_anchor_positive_input",
+    "generate_negative_input",
+    "positive_criterion_for_triplet_loss",
+    "negative_criterion_for_triplet_loss",
+    "calculate_triplet_loss",
 ]
 
 
