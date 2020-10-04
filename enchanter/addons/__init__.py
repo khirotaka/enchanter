e40c0474b1a65bcb383067978abbc7bed82fda4c
Hirotaka Kawashima <k.hirotaka@me.com>
2020-10-04 10:20:43 +0900

    add criterions

--- enchanter/addons/__init__.py
+++ enchanter/addons/__init__.py
@@ -1,6 +1,7 @@
 from .activation import *
 from .optim_wrapper import *
 from . import layers
+from . import criterions
 
 
-__all__ = ["Mish", "Swish", "mish", "TransformerOptimizer", "layers"]
+__all__ = ["Mish", "Swish", "mish", "TransformerOptimizer", "layers", "criterions"]
