ec1dfdf224937ac4fd7821c7c0a0e98187f65b27
Hirotaka Kawashima <k.hirotaka@me.com>
2020-10-10 23:39:59 +0900

    #108 1d ResNet series (Temporal Convolution Block)

--- enchanter/addons/layers/__init__.py
+++ enchanter/addons/layers/__init__.py
@@ -1,4 +1,4 @@
-from .conv import CausalConv1d
+from .conv import CausalConv1d, TemporalConvBlock
 from .dense_interpolation import DenseInterpolation
 from .mlp import AutoEncoder, MLP, PositionWiseFeedForward, ResidualSequential
 from .positional_encoding import PositionalEncoding
@@ -8,6 +8,7 @@ from .se_layer import SELayer1d, SELayer2d
 __all__ = [
     "AutoEncoder",
     "CausalConv1d",
+    "TemporalConvBlock",
     "DenseInterpolation",
     "MLP",
     "PositionalEncoding",
