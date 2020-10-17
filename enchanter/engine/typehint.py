41c68cc50a357a7105defa61324eedd0d2d8f8d1
Hirotaka Kawashima <k.hirotaka@me.com>
2020-10-17 15:33:10 +0900

    add method

--- enchanter/engine/typehint.py
+++ enchanter/engine/typehint.py
@@ -24,3 +24,13 @@ class ScikitModel(Protocol):
     def set_params(self, **params):
         # pylint: disable=W0104
         ...
+
+    # pylint: disable=R0201
+    def decision_function(self, X):
+        # pylint: disable=W0104
+        ...
+
+    # pylint: disable=R0201
+    def get_params(self, deep=True):
+        # pylint: disable=W0104
+        ...
