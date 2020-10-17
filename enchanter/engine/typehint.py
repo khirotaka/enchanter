b37eacb38506f3c665bbc52a078edb5fbb9696d6
Hirotaka Kawashima <k.hirotaka@me.com>
2020-10-17 13:22:06 +0900

    ignore R0201

--- enchanter/engine/typehint.py
+++ enchanter/engine/typehint.py
@@ -5,14 +5,18 @@ __all__ = ["ScikitModel"]
 
 
 class ScikitModel(Protocol):
+    # pylint: disable=R0201
     def fit(self, X, y, sample_weight=None):
         ...
 
+    # pylint: disable=R0201
     def predict(self, X):
         ...
 
+    # pylint: disable=R0201
     def score(self, X, y, sample_weight=None):
         ...
 
+    # pylint: disable=R0201
     def set_params(self, **params):
         ...
